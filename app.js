9b972b781c74f89d1a6b47da464a49944d468913
Petr Janda <petrjanda@me.com>
2011-12-26 22:15:16 +0100

    more experiments with even bus

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-var CommandBus = require('./lib/commandBus'),
-    CommandHandler = require('./lib/commandHandler');
+var EventBus = require('./lib/eventBus');
+var db = require('./lib/storage');
 
-commandBus = new CommandBus();
-commandBus.registerHandler( 'foo', new CommandHandler() );
+eventBus = new EventBus();
+// eventBus.save( 'FirstEventCreated', 1, {foo: 'bar'} );
+// eventBus.save( 'SecondEventCreated', 1 );
+// eventBus.save( 'ThirdEventCreated', 1 );
+// eventBus.save( 'ForthEventCreated', 1 );
+// eventBus.storeSnapshot( 1, 2, {foo: 'bar'} );
 
-commandBus.execute( 'foo' );
+eventBus.loadData( 1, function(snapshot, events) {
+  console.log('snapshot:');
+  console.log(snapshot);
+
+  console.log('events:');
+  console.log(events);
+});
+
+//db.edb.quit();
