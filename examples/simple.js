281bac2314426127c64aa3728aeb71be7f6c71a5
Petr Janda <petrjanda@me.com>
2012-01-05 00:05:43 +0100

    slightly update most simple use case

--- examples/simple.js
+++ examples/simple.js
@@ -3,16 +3,23 @@ var util = require('util'),
 
 var Foo = function(id, callback) {
   Aggregate.call(this, id, callback);
+
+  this.sentence = '';
 }
 
 util.inherits(Foo, Aggregate);
 
 Foo.prototype.apply = function(event) {
-  console.log(event);
+  switch(event.name) {
+    case 'foobar':
+      this.sentence += ' ' + event.attrs.foo;
+      break;
+  }
 }
 
 var foo = new Foo(1, function() {
   console.log('Ready!');
+  console.log(this.sentence);
 })
 
 foo.emit('foobar', {foo: 'bar'})
