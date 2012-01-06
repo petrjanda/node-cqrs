e220be57349ef51942e36019d73f23c65e68b786
Petr Janda <petrjanda@me.com>
2012-01-06 22:10:36 +0100

    test cover command bus

---
+++ spec/commandBusSpec.js
@@ -0,0 +1,41 @@
+var CommandBus = require('../lib/commandBus');
+
+describe('CommandBus', function() {
+  var commandBus;
+
+  beforeEach(function() {
+    commandBus = new CommandBus();
+  })
+
+  it('should initialize empty handlers list', function() {
+    expect(commandBus.handlers).toEqual({});
+  })
+
+  describe('.registerHandler', function() {
+    it('should register handler function', function() {
+      var f = function() {}
+
+      commandBus.registerHandler('event', f);
+
+      expect(commandBus.handlers['event']).toEqual(f);
+    })  
+  })
+
+  describe('.execute', function() {
+    it('should throw an error in case handler doesnt exist', function() {
+      expect(function() { 
+        commandBus.execute('command'); 
+      }).toThrow('Handler for \'command\' doesn\'t exist!');
+    })
+
+    it('should call handler if available', function() {
+      commandBus.handlers['foo'] = function() {}
+      spyOn(commandBus.handlers, 'foo');
+
+      commandBus.execute('foo', {foo: 'bar'});
+
+      expect(commandBus.handlers['foo']).toHaveBeenCalledWith({foo: 'bar'});
+    })
+  })
+  
+})
