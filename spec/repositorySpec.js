b663f6690847dcefc3f2a4f34cae898a04633038
Petr Janda <petrjanda@me.com>
2012-01-07 20:15:17 +0100

    repository store the strategy

--- spec/repositorySpec.js
+++ spec/repositorySpec.js
@@ -1,8 +1,18 @@
 var Repository = require('../lib/repository');
 
 describe('Repository', function() {
+  var repository;
+
+  beforeEach(function() {
+    repository = new Repository('foo');
+  })
+
   it('should exists', function() {
     expect(Repository).toBeDefined();
     expect(typeof Repository).toEqual('function');
+  })
+
+  it('should store the strategy object', function() {
+    expect(repository.strategy).toEqual('foo');
   })
 })
