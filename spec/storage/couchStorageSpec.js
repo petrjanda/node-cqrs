518cdd0a5056cf60d09f8fb611ebc680ee4cd53f
Petr Janda <petrjanda@me.com>
2012-01-11 00:50:25 +0100

    couch view storage on the way

---
+++ spec/storage/couchStorageSpec.js
@@ -0,0 +1,7 @@
+var CouchStorage = require('../../lib/storage/couch')
+
+describe('CouchStorage', function() {
+  it('should be defined', function() {
+    expect(typeof CouchStorage).toEqual('function');
+  })
+})
