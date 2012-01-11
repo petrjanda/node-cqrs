27ca76ee2c4f9999ecbf5786141103edb2c01e3e
Petr Janda <petrjanda@me.com>
2012-01-11 18:04:21 +0100

    update and cover view

--- spec/storage/couchStorageSpec.js
+++ spec/storage/couchStorageSpec.js
@@ -1,7 +1,42 @@
-var CouchStorage = require('../../lib/storage/couch')
+var CouchStorage = require('../../lib/storage/couch'),
+    jasmine = require('jasmine-node');
 
 describe('CouchStorage', function() {
-  it('should be defined', function() {
-    expect(typeof CouchStorage).toEqual('function');
+  var couchStorage;
+
+  beforeEach(function() {
+    couchStorage = new CouchStorage();  
+  })
+
+  describe('instance', function() {
+    it('should get instance of CouchStorage', function() {
+      var couchStorage = CouchStorage.getInstance()
+      expect(typeof couchStorage.request).toEqual('function');
+    })
+
+    it('should return just one instance', function() {
+      expect(CouchStorage.getInstance()).toEqual(CouchStorage.getInstance())
+    })
+  })
+
+  describe('.storeView', function() {
+    it('should call createDocument', function() {
+      var view = {uid: 1, lastEvent: 12, data: 'foo'};
+      spyOn(couchStorage, 'createDocument');
+
+      couchStorage.storeView(view);
+
+      expect(couchStorage.createDocument).toHaveBeenCalledWith('{"viewId":1,"type":"view","lastEvent":12,"data":"foo"}');
+    })
+  })
+
+  describe('.loadView', function() {
+    it('should call request', function() {
+      spyOn(couchStorage, 'request');
+
+      couchStorage.loadView('f4f5g8e76tko');
+
+      expect(couchStorage.request).toHaveBeenCalledWith({ method : 'GET', path : '/cqrs/f4f5g8e76tko' }, jasmine.any(Function));
+    })
   })
 })
