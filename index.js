fc79f2261b11d7bf00c5bf1afae26f8b7991139c
Petr Janda <petrjanda@me.com>
2012-01-15 23:23:35 +0100

    publish couch storage and repository

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var Aggregate = require('./lib/aggregate'),
     CommandBus = require('./lib/commandBus'),
     View = require('./lib/view'),
-    Repository = require('./lib/repository');
+    Repository = require('./lib/repository'),
+    CouchRepository = require('./lib/repository/couchRepository'),
+    CouchStorage = require('./lib/strage/couchStorage');
 
 module.exports.Aggregate = Aggregate;
 module.exports.CommandBus = CommandBus;
 module.exports.View = View;
 module.exports.Repository = Repository;
+
+module.exports.CouchRepository = CouchRepository;
+module.exports.CouchStorage = CouchStorage;
