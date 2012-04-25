4779db7919a7c7b2e9e388695d0e0c9f054c4bf7
Petr Janda <petrjanda@me.com>
2012-04-25 22:56:33 +0200

    add denormalizer component

---
+++ spec/denormalizerSpec.js
@@ -0,0 +1,71 @@
+var Denormalizer = require('../lib/denormalizer');
+
+describe('Denormalizer', function() {
+  var denormalizer = null;
+
+  beforeEach(function() {
+    denormalizer = new Denormalizer();
+  })
+
+  describe('#registerView', function() {
+
+    it('should store view in the list of views', function() {
+      var view = new View('foo', ['bar', 'baz']);
+
+      denormalizer.registerView(view);
+
+      expect(denormalizer.views['foo']).toEqual(view);
+    })
+
+    it('should store view to the events map', function() {
+      var view = new View('foo', ['bar', 'baz']);
+
+      denormalizer.registerView(view);
+
+      expect(denormalizer.eventsMap['bar']).toEqual([view]);
+      expect(denormalizer.eventsMap['baz']).toEqual([view]);
+    })
+
+    it('should throw an exception if the view already exists', function() {
+      var view = new View('foo', ['bar', 'baz']);
+
+      denormalizer.registerView(view);
+
+      expect(function() { 
+        denormalizer.registerView(view);
+      }).toThrow('View with a given name is already registered!');
+    })
+  })
+
+  describe('#updateViews', function() {
+    it('should trigger .load function for all registered views', function() {
+      var view = new View('foo', ['bar', 'baz']);
+      spyOn(view, 'load');
+
+      denormalizer.registerView(view);
+
+      denormalizer.updateViews('baz');
+
+      expect(view.load).toHaveBeenCalled();
+    })
+  })
+
+  describe('#build', function() {
+    it('should trigger .load function for the view if exists', function() {
+      var view = new View('foo', ['bar', 'baz']);
+      spyOn(view, 'load');
+
+      denormalizer.registerView(view);
+
+      denormalizer.build('foo');
+
+      expect(view.load).toHaveBeenCalled();
+    })
+
+    it('should throw exception if view doesnt exist', function() {
+      expect(function() { 
+        denormalizer.build('foo');
+      }).toThrow('View is not registered!');
+    })
+  })
+})
