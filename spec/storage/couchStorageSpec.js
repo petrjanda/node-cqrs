deb3ad39a683c4a7b83d85cf84feb8875bd6bffe
Petr Janda <petrjanda@me.com>
2012-04-30 22:33:08 +0200

    update setup methods, improve view snapshot ids, fix global variables issue

--- spec/storage/couchStorageSpec.js
+++ spec/storage/couchStorageSpec.js
@@ -37,7 +37,9 @@ describe('CouchStorage', function() {
   }
 
   function viewPath(id) {
-    return '/cqrs/_design/cqrs/_view/viewSnapshot?startkey=["' + id + '","999999999999999999"]&endkey=["' + id + '","0"]&limit=1&descending=true'
+    return '/cqrs/_design/cqrs/_view/viewSnapshot?startkey=["' + 
+      id + '","999999999999999999"]&endkey=["' + 
+      id + '","0"]&limit=1&descending=true';
   }
 
   beforeEach(function() {
@@ -57,13 +59,23 @@ describe('CouchStorage', function() {
 
   describe('.storeView', function() {
     it('should call createDocument', function() {
-      var view = {uid: 1, lastEvent: 12, data: 'foo'};
+      var view = {uid: 'name', lastEvent: 12, data: 'foo'};
       spyOn(couchStorage, 'createDocument');
       spyOn(utils, 'uuid').andReturn(123456);
 
       couchStorage.storeView(view);
 
-      expect(couchStorage.createDocument).toHaveBeenCalledWith(123456, {viewId:1,type:"view",lastEvent:12,time:123456,data:"foo"});
+      expect(couchStorage.createDocument).toHaveBeenCalledWith(
+        'name-123456', 
+        {
+          viewId: 'name',
+          type: "view",
+          lastEvent: 12,
+          time: 123456,
+          data: "foo"
+        },
+        jasmine.any(Function)
+      );
     })
   })
 
@@ -74,7 +86,11 @@ describe('CouchStorage', function() {
 
       couchStorage.purgeView('1j0ddsesdfsfdo0m7');
 
-      expect(couchStorage.deleteDocument).toHaveBeenCalledWith('e9f59b5f8c965ebce700eeec1baf7a60', '1-0145acc24d7c96db4d8ef260ad4afec4');
+      expect(couchStorage.deleteDocument).toHaveBeenCalledWith(
+        'e9f59b5f8c965ebce700eeec1baf7a60', 
+        '1-0145acc24d7c96db4d8ef260ad4afec4'
+      );
+
       expect(couchStorage.request).toHaveBeenCalledWith({ 
         method : 'GET', 
         path : viewPath('1j0ddsesdfsfdo0m7') }, 
