a84c2c05292ac938359a7a3ebc859e4053e9ccc0
Petr Janda <petrjanda@me.com>
2012-05-20 23:39:53 +0200

    export view builder

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Aggregate = require('./lib/aggregate'),
     CommandBus = require('./lib/commandBus'),
     View = require('./lib/view'),
+    ViewBuilder = require('./lib/viewBuilder'),
     Repository = require('./lib/repository'),
     CouchRepository = require('./lib/repository/couchRepository'),
     CouchStorage = require('./lib/storage/couchStorage'),
@@ -9,6 +10,7 @@ var Aggregate = require('./lib/aggregate'),
 module.exports.Aggregate = Aggregate;
 module.exports.CommandBus = CommandBus;
 module.exports.View = View;
+module.exports.ViewBuilder = ViewBuilder;
 module.exports.Repository = Repository;
 module.exports.Denormalizer = Denormalizer;
 
